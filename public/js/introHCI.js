16d919f02a699399c20b11043a7f48a714e9bae8
emilyferguson989 <emilyferguson989@gmail.com>
2020-02-13 10:50:42 -0800

    submitting

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,6 +25,12 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	var result = $.get("/project/" + idNumber, callBackFn("/project/" + idNumber));
 
 	console.log("User clicked on project " + idNumber);
+}
+
+function callBackFn(result) {
+	console.log(result);
+
 }
